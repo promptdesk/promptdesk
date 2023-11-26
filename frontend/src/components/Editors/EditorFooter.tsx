e2c174a8b552948ce1f8437d836bbd51d966fdb8
Bradley Allen Arsenault <bradleyallenarsenault@gmail.com>
2023-11-26 15:01:28 -0500

    Added in a clear button to the completion page to delete the completion result.

--- frontend/src/components/Editors/EditorFooter.tsx
+++ frontend/src/components/Editors/EditorFooter.tsx
@@ -1,80 +1,95 @@
 "use client";
-import React, { useEffect, useState } from 'react';
-import { promptWorkspaceTabs } from "@/stores/TabStore";
-import { generateResultForPrompt } from "@/services/GenerateService";
+import React, {useEffect, useState} from 'react';
+import {promptWorkspaceTabs} from "@/stores/TabStore";
+import {generateResultForPrompt} from "@/services/GenerateService";
 import CodeModal from "@/components/Modals/CodeModal";
 import PlaygroundButton from "@/components/Form/PlaygroundButton";
 import {
     shouldShowSaveModal,
     shouldShowCodeModal
-  } from "@/stores/GeneralStore";
+} from "@/stores/GeneralStore";
 import Link from "next/link";
 import {useRouter} from "next/router";
+
 function EditorFooter() {
-  const {push, query} = useRouter();
-  const { getDataById, activeTabId, tabs } = promptWorkspaceTabs();
-  const [ data, setData ] = useState(getDataById(activeTabId as string) || {});
+    const {push, query} = useRouter();
+    const {getDataById, activeTabId, tabs} = promptWorkspaceTabs();
+    const [data, setData] = useState(getDataById(activeTabId as string) || {});
 
-  useEffect(() => {
-    const data = getDataById(activeTabId as string) || {};
-    setData(data);
-  }, [activeTabId, getDataById, tabs]);
+    useEffect(() => {
+        const data = getDataById(activeTabId as string) || {};
+        setData(data);
+    }, [activeTabId, getDataById, tabs]);
 
-  const {
-      toggle_modal
+    const {
+        toggle_modal
     } = shouldShowSaveModal();
 
     const {
-      show_code_modal,
-      toggle_code_modal
+        show_code_modal,
+        toggle_code_modal
     } = shouldShowCodeModal();
 
     const goToSamplesPage = () => {
         push(`/workspace/${activeTabId}/samples`);
     }
 
-  return (
-    <div className="pg-content-footer">
-      <hr></hr>
-      <div className="pg-footer-left">
-        <button
-          id="submit-prompt"
-          tabIndex={0}
-          className={`btn btn-sm btn-filled ${
-            data.loading ? 'btn-neutral' : 'btn-primary'
-          } pg-submit-btn`}
-          type="button"
-          aria-haspopup="true"
-          aria-expanded="false"
-        >
+    const handleClearResultClicked = () => {
+        promptWorkspaceTabs.getState().updateDataById(activeTabId as string, {loading: false, generatedText: null})
+    };
+
+    return (
+        <div className="pg-content-footer">
+            <hr></hr>
+            <div className="pg-footer-left">
+                <button
+                    id="submit-prompt"
+                    tabIndex={0}
+                    className={`btn btn-sm btn-filled ${
+                        data.loading ? 'btn-neutral' : 'btn-primary'
+                    } pg-submit-btn`}
+                    type="button"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                >
         <span className="btn-label-wrap">
             <span className="btn-label-inner" onClick={() => {
-              generateResultForPrompt(activeTabId as string);
+                generateResultForPrompt(activeTabId as string);
             }}>
             {data.loading ? "Processing..." : "Submit"}
             </span>
         </span>
-        </button>
-        <PlaygroundButton
-              text="Save"
-              id="save-prompt"
-              onClick={toggle_modal}
-              isFull={true}
-        />
-        <PlaygroundButton
-            text="Code"
-            onClick={toggle_code_modal}
-            isFull={true}
-        />
-        <PlaygroundButton
-            text="Samples"
-            onClick={goToSamplesPage}
-            isFull={true}
-        />
-    </div>
-    {show_code_modal && <CodeModal />}
-    </div>
-  )
+                </button>
+                <PlaygroundButton
+                    text="Save"
+                    id="save-prompt"
+                    onClick={toggle_modal}
+                    isFull={true}
+                />
+                <PlaygroundButton
+                    text="Code"
+                    onClick={toggle_code_modal}
+                    isFull={true}
+                />
+                <PlaygroundButton
+                    text="Samples"
+                    onClick={goToSamplesPage}
+                    isFull={true}
+                />
+                {
+                    data.generatedText ?
+                        <PlaygroundButton
+                            text="Clear Result"
+                            id="clear-result"
+                            onClick={handleClearResultClicked}
+                            isFull={true}
+                        />
+                        : null
+                }
+            </div>
+            {show_code_modal && <CodeModal/>}
+        </div>
+    )
 
 }
 
