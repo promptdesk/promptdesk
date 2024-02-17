1af6e2e92ee45189a9eaba91163fab82311c5647
seattlereactdev <98289721+seattlereactdev@users.noreply.github.com>
2024-02-17 07:53:19 -0800

    Refactor CustomJSONView to destructure props

--- frontend/src/components/Viewers/CustomJSONView.tsx
+++ frontend/src/components/Viewers/CustomJSONView.tsx
@@ -1,16 +1,15 @@
 import JsonView from "react18-json-view";
 import "react18-json-view/src/style.css";
-
 import "./CustomJSONView.scss";
 
-export function CustomJSONView(props: any) {
+export function CustomJSONView({ theme = "shapeshifter:inverted", displayObjectSize = false, displayDataTypes = false, sortKeys = true, ...props }: any) {
   return (
     <div className={"custom-json-view"}>
       <JsonView
-        theme={"shapeshifter:inverted"}
-        displayObjectSize={false}
-        displayDataTypes={false}
-        sortKeys={true}
+        theme={theme}
+        displayObjectSize={displayObjectSize}
+        displayDataTypes={displayDataTypes}
+        sortKeys={sortKeys}
         {...props}
       />
     </div>
