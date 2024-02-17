0e2e53292662fe92475f8798ba27264143462f74
Justin Macorin <justinmacorin@gmail.com>
2024-02-16 17:04:19 -0800

    added test

--- backend/src/models/mongodb/db.ts
+++ backend/src/models/mongodb/db.ts
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, "../../../../.env") });
 
 export default async function connectToDatabase(test = false) {
   console.log("INFO :: MONGODB CONNECTING", process.env.MONGO_URL);
